02f3ace2f57d647bcbf887f76ecad21e2b517cbb
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-04-20 15:51:21 +0200

    Simplify BaseAdapter classes

--- commons/ui/src/main/kotlin/com/jpaya/commons/ui/base/BaseListAdapter.kt
+++ commons/ui/src/main/kotlin/com/jpaya/commons/ui/base/BaseListAdapter.kt
@@ -1,6 +1,5 @@
 package com.jpaya.commons.ui.base
 
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
@@ -11,10 +10,5 @@ import androidx.recyclerview.widget.RecyclerView
  * @param contentsSame Function called to check whether two items have the same data.
  * @see ListAdapter
  */
-abstract class BaseListAdapter<T>(
-    itemsSame: (T, T) -> Boolean,
-    contentsSame: (T, T) -> Boolean
-) : ListAdapter<T, RecyclerView.ViewHolder>(object : DiffUtil.ItemCallback<T>() {
-    override fun areItemsTheSame(old: T, new: T): Boolean = itemsSame(old, new)
-    override fun areContentsTheSame(old: T, new: T): Boolean = contentsSame(old, new)
-})
+abstract class BaseListAdapter<T>(itemsSame: (T, T) -> Boolean, contentsSame: (T, T) -> Boolean) :
+    BaseViewHolderListAdapter<T, RecyclerView.ViewHolder>(itemsSame, contentsSame)

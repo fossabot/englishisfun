f677fff6413b1698bf68e0f8f758c4c08462f054
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-04-30 13:15:56 +0200

    Update build.gradle.kts [skip ci]

--- libraries/test_utils/build.gradle.kts
+++ libraries/test_utils/build.gradle.kts
@@ -16,7 +16,7 @@
 
 import dependencies.AnnotationProcessorsDependencies
 import dependencies.Dependencies
-import dependencies.TestDependencies
+import extensions.addTestsDependencies
 import extensions.implementation
 
 plugins {
@@ -27,19 +27,7 @@ dependencies {
     implementation(Dependencies.PAGING)
     implementation(Dependencies.NAVIGATION_UI)
 
-    implementation(TestDependencies.JUNIT)
-    implementation(TestDependencies.MOCKITO)
-    implementation(TestDependencies.ASSERTJ)
-    implementation(TestDependencies.ROBOELECTRIC)
-    implementation(TestDependencies.ROOM)
-    implementation(TestDependencies.CORE)
-    implementation(TestDependencies.ARCH_CORE)
-    implementation(TestDependencies.RULES)
-    implementation(TestDependencies.RUNNER)
-    implementation(TestDependencies.COROUTINES_TEST)
-    implementation(TestDependencies.FRAGMENT_TEST)
-    implementation(TestDependencies.EXT)
-    implementation(TestDependencies.MOCK_WEB_SERVER)
+    addTestsDependencies()
 
     annotationProcessor(AnnotationProcessorsDependencies.AUTO_SERVICE)
 }

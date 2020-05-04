feea60e0d0fe078ee2cba4e3c537ed6abb831364
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-05-04 16:17:33 +0200

    Fix test errors

--- features/characters_list/src/test/kotlin/com/jpaya/dynamicfeatures/characterslist/ui/list/adapter/CharactersListAdapterStateTest.kt
+++ features/characters_list/src/test/kotlin/com/jpaya/dynamicfeatures/characterslist/ui/list/adapter/CharactersListAdapterStateTest.kt
@@ -16,13 +16,13 @@
 
 package com.jpaya.dynamicfeatures.characterslist.ui.list.adapter
 
-import junit.framework.Assert.assertFalse
-import junit.framework.Assert.assertTrue
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class CharactersListAdapterStateTest {
 
-    lateinit var state: CharactersListAdapterState
+    private lateinit var state: CharactersListAdapterState
 
     @Test
     fun setStateAsAdded_ShouldBeSettled() {

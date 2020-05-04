fd2d8f6da573118cec71f348ec8238735b1155ac
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-05-04 15:52:13 +0200

    Create AbbreviationsModuleTest

---
+++ features/abbreviations/src/test/kotlin/com/jpaya/dynamicfeatures/abbreviations/ui/di/AbbreviationsModuleTest.kt
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2020 Jose Maria Payá Castillo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jpaya.dynamicfeatures.abbreviations.ui.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.firestore.FirebaseFirestore
+import com.jpaya.commons.ui.extensions.viewModel
+import com.jpaya.dynamicfeatures.abbreviations.ui.AbbreviationsListFragment
+import com.jpaya.dynamicfeatures.abbreviations.ui.AbbreviationsListViewModel
+import com.jpaya.dynamicfeatures.abbreviations.ui.model.AbbreviationItemMapper
+import com.jpaya.dynamicfeatures.abbreviations.ui.paging.AbbreviationsPageDataSourceFactory
+import io.mockk.*
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.CoroutineScope
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class AbbreviationsModuleTest {
+
+    @MockK
+    lateinit var fragment: AbbreviationsListFragment
+    private lateinit var module: AbbreviationsModule
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun initializeCharactersListModule_ShouldSetUpCorrectly() {
+        module = AbbreviationsModule(fragment)
+
+        assertEquals(fragment, module.fragment)
+    }
+
+    @Test
+    fun verifyProvidedAbbreviationsListViewModel() {
+        mockkStatic("com.jpaya.commons.ui.extensions.FragmentExtensionsKt")
+
+        every {
+            fragment.viewModel(any(), any<() -> ViewModel>())
+        } returns mockk<AbbreviationsListViewModel>()
+
+        val factoryCaptor = slot<() -> AbbreviationsListViewModel>()
+        val dataFactory = mockk<AbbreviationsPageDataSourceFactory>(relaxed = true)
+        module = AbbreviationsModule(fragment)
+        module.providesAbbreviationsListViewModel(dataFactory)
+
+        verify { fragment.viewModel(factory = capture(factoryCaptor)) }
+
+        assertEquals(dataFactory, factoryCaptor.captured().dataSourceFactory)
+    }
+
+    @Test
+    fun verifyProvidedCharactersPageDataSource() {
+        val firestore = mockk<FirebaseFirestore>(relaxed = true)
+        val mapper = mockk<AbbreviationItemMapper>(relaxed = true)
+        val viewModel = mockk<AbbreviationsListViewModel>(relaxed = true)
+        val scope = mockk<CoroutineScope>()
+        every { viewModel.viewModelScope } returns scope
+
+        module = AbbreviationsModule(fragment)
+        val dataSource = module.providesAbbreviationsPageDataSource(
+            viewModel = viewModel,
+            firestore = firestore,
+            mapper = mapper
+        )
+
+        assertEquals(firestore, dataSource.firestore)
+        assertEquals(mapper, dataSource.mapper)
+        assertEquals(scope, dataSource.scope)
+    }
+
+    @Test
+    fun verifyProvidedAbbreviationsListAdapter() {
+        val viewModel = mockk<AbbreviationsListViewModel>(relaxed = true)
+
+        module = AbbreviationsModule(fragment)
+        val adapter = module.providesAbbreviationsListAdapter(viewModel)
+
+        assertEquals(viewModel, adapter.viewModel)
+    }
+}

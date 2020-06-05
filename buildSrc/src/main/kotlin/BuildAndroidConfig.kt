3968dc7c79a896e643bf4f20c6db7694caa7ef37
Jose Paya <josepaya17@gmail.com>
2020-06-05 23:42:47 +0200

    Update version code

--- buildSrc/src/main/kotlin/BuildAndroidConfig.kt
+++ buildSrc/src/main/kotlin/BuildAndroidConfig.kt
@@ -9,7 +9,7 @@ object BuildAndroidConfig {
     const val MIN_SDK_VERSION = 21
     const val TARGET_SDK_VERSION = 29
 
-    const val VERSION_CODE = 3
+    const val VERSION_CODE = 4
     const val VERSION_NAME = "0.0.3"
 
     const val SUPPORT_LIBRARY_VECTOR_DRAWABLES = true
